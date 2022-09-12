d8b4dcf812f9704a1b9b3b06fcd57355e03f32a4
vitor4breu <vitor148197@hotmail.com>
2022-09-12 17:54:51 -0300

    Updates

---
+++ precache-manifest.8e1a9faf7b0586cede5220fc935db267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a152f0c7e21d36197a15",
+    "url": "/static/css/main.9e83e642.chunk.css"
+  },
+  {
+    "revision": "a152f0c7e21d36197a15",
+    "url": "/static/js/main.a152f0c7.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3514753640b5003b0f9df55a389ee5fd",
+    "url": "/index.html"
+  }
+];
